54a9691e5f578b64f28ec1f51b907bbb5dd24cb8
YikunHuang123 <mac@mmmmmac.lan>
2026-05-27 23:39:28 +0200

    Delete all contents

--- README.md
+++
@@ -1,6 +0,0 @@
-# 没有数据集
-/chinese_rbtl3_L-3_H-1024_A-16
-
-/data
-
-/modules/bert_module_demo/data
--- controllers/index.py
+++
@@ -1,9 +0,0 @@
-import flask
-from flask import Blueprint, request, render_template
-
-index_page = Blueprint("index_page", __name__)
-
-
-@index_page.route("/", methods=["post", "GET"])
-def hello():
-    return render_template('index.html')
